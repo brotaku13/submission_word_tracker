c9d89e0c45ac276f3a7ed0d6f80e3eb02b1a1a68
brotaku13 <brian.caulfield13@gmail.com>
2017-05-11 15:48:37 -0700

    added comments

--- word_tracker_main.py
+++ word_tracker_main.py
@@ -2,7 +2,14 @@ import config
 import praw
 from collections import Counter
 
+
+
 def login():
+    """
+    creates a reddit instance. id, client secret etc are private information and therefore not uploaded to github.
+    Param: none
+    return: reddit instance as r
+    """
     print('logging in...')
     r = praw.Reddit(client_id=config.client_id,
                     client_secret=config.client_secret,
@@ -13,12 +20,19 @@ def login():
     return r
 
 
-"""
-used when wanting to search a specific subreddit for a specific subject and user
-"""
+
 def search_bill_gates(r):
+    """
+    used when wanting to search a specific subreddit for a specific subject and user
+    param: reddit instance
+    returns a list of comment ID's
+    """
+
 
     ama_post_list = []
+
+    # for post in a given subreddit (IAmA) search the subreddit for posts with the word 'Bill Gates'.
+    # if the post author is 'thisisbillgates', append the post ID to a list and return it
     for post in r.subreddit('IAmA').search('bill gates', sort='relevance', syntax='cloudsearch', time_filter='all'):
         if post.author.name == 'thisisbillgates':
             ama_post_list.append(post.id)
@@ -26,6 +40,12 @@ def search_bill_gates(r):
     return ama_post_list
 
 def user_words(r, username):
+    """
+    parses though words in the profile of a users post history and creates and edits a txt file of all words in last 1000 posts
+    param: reddit instance, given username
+    returns void
+    """
+
     # collects words from all comments (last 1000?) and puts them in a text file
     with open("{} comments.txt".format(username), 'w', encoding='utf-8') as f:
         comment_count=0
@@ -44,6 +64,13 @@ def user_words(r, username):
 
 
 def word_count(username):
+    """
+    opens a txt file and processes the each post into a list of words.
+    counts words and prints out most used words (in this case, top 50)
+    param: reddit instance, given username
+    returns void
+    """
+
     f = open("{} comments.txt".format(username), 'r')
     filetext = f.read()
     # replaces
@@ -60,7 +87,7 @@ def word_count(username):
     long_word_list = [word for word in word_list if len(word) > 7]
 
     word_count = Counter(long_word_list)
-    print("Most comon")
+    print("Most common")
     for key, value in word_count.most_common(50):
         print('{} : {}'.format(key, value))
 
